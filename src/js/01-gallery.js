256231dbcdb5e9647eca9e61282f979125db110d
SvetlanaRomaniuk111 <126294774+SvetlanaRomaniuk111@users.noreply.github.com>
2023-07-14 23:02:18 +0300

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const createGelery = galleryItems.map(
+  ({ preview, original, description }) =>
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-src="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`
+);
+
+gallery.insertAdjacentHTML('beforeend', createGelery.join(''));
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
