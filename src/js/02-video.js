daf9885d8223578a5b92eeb4736dea6f9140ba15
SvetlanaRomaniuk111 <126294774+SvetlanaRomaniuk111@users.noreply.github.com>
2023-07-19 14:42:26 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    console.log('--->>> here <<<---');
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }, 1000)
+);
+player.setCurrentTime(Number(localStorage.getItem('videoplayer-current-time')));
